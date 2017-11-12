6d4d38a6d6559e7d08e54540b2ec319f7f639b0b
Sidharth Samant <sidharthsamant883@gmail.com>
2017-11-12 12:17:49 +0530

    Add test

--- routeyourfood/tests.py
+++ routeyourfood/tests.py
@@ -2,5 +2,12 @@
 from __future__ import unicode_literals
 
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class FrontViewTests(TestCase):
+	def test_front_view(self):
+		response = self.client.get(reverse('front'))
+		self.assertEqual(response.status_code, 200)
+		
