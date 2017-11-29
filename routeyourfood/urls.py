c688cd88c9bc05865b528a71c58a396cb73e5e68
Sidharth Samant <sidharthsamant883@gmail.com>
2017-11-29 16:33:51 +0530

    Add name kwarg to FrontView in urls.py

--- routeyourfood/urls.py
+++ routeyourfood/urls.py
@@ -7,7 +7,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
 	# url(r'^$', views.FrontView.as_view(), name='front'),
 	# url(r'^$', TemplateView.as_view(template_name="routeyourfood/front.html")),
-	url(r'^$', views.FrontView.as_view()),
+	url(r'^$', views.FrontView.as_view(), name='front'),
 	# url(r'^$', views.front, name='front'),
 	# url(r'^details/(?P<page>[0-9]+)/(?P<order>[a-z\-]+)/$', views.details, name='detailspage'),
 	url(r'^details/(?P<page>[0-9]+)/(?P<order>[a-z\-]+)/$', views.RestaurantListView.as_view(), name='detailspage'),
